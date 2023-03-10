b6ce215079d64b0a9d833879f39140fe62fb62bd
karol <karol05ks@gmail.com>
2023-03-10 17:15:52 +0100

    -setup workspace

---
+++ src/main/java/com/academy/speedTyper/SpeedTyperApplication.java
@@ -0,0 +1,13 @@
+package com.academy.speedTyper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedTyperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedTyperApplication.class, args);
+	}
+
+}
